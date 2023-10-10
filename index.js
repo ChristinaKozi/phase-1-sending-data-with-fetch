024deabc9ad49e67c91e79c434d151e7440a8cd5
Christina Kozanian <christinakozanian@gmail.com>
2023-10-10 00:47:58 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+const submitData = (name, email) => {
+   return fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify({name, email})
+    })
+    .then(res => res.json())
+    .then(user => {
+        const newID = user.id;
+        const idElement =  document.createElement('p')
+        idElement.textContent = `${newID}`
+        document.body.appendChild(idElement)
+    })
+    .catch(error => {
+        alert("Post failed")
+        const errorMessage = error.message;
+        const errorDom = document.createElement('p')
+        errorDom.textContent = errorMessage
+        document.body.appendChild(errorDom)
+    });
+}
+
+
